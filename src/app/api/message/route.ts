0057288196697e7ed4de787d72311fec3fe25f7a
Kenny Chow <68889558+kennychowpd@users.noreply.github.com>
2023-05-02 22:42:07 +0800

    Building openai-stream

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,17 +1,41 @@
 import { MessageArraySchema } from "@/lib/validators/message";
-import {ChatGPTMessage} from "@/lib/openai-stream"
+import {
+	ChatGPTMessage,
+	OpenAIStream,
+	OpenAIStreamPayload,
+} from "@/lib/openai-stream";
+import { chatbotPrompt } from "@/app/helpers/constants/chatbot-prompt";
 
 export async function POST(req: Request) {
 	const { messages } = await req.json();
 
 	const parsedMessages = MessageArraySchema.parse(messages);
 
-  const outboundMessages: ChatGPTMessage[] = parsedMessages.map((message) => ({
-		role: message.isUserMessage ? "user" : "system",
-		content: message.text,
-	}))
+	const outboundMessages: ChatGPTMessage[] = parsedMessages.map(
+		(message) => ({
+			role: message.isUserMessage ? "user" : "system",
+			content: message.text,
+		})
+	);
 
 	outboundMessages.unshift({
 		role: "system",
-		content: chatbotPrompt
-	})
+		content: chatbotPrompt,
+	});
+
+	const payload: OpenAIStreamPayload = {
+		model: "gpt-3.5-turbo",
+		messages: outboundMessages,
+		temperature: 0.4,
+		// top_p:1, #commented out following openai doc [!(temperature && top_p)]
+		frequency_penalty: 0,
+		presence_penalty: 0,
+		max_tokens: 150,
+		stream: true,
+		n: 1,
+	};
+
+	const stream = await OpenAIStream(payload);
+
+	return new Response(stream);
+}
